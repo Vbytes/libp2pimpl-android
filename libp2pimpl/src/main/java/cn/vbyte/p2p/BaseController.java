9e2399166c9a4c490ed15db5f82e65b032a8e8e4
dengpan <dp199263@hotmail.com>
2017-03-29 17:41:21 +0800

    修改异步实现

--- libp2pimpl/src/main/java/cn/vbyte/p2p/BaseController.java
+++ libp2pimpl/src/main/java/cn/vbyte/p2p/BaseController.java
@@ -1,5 +1,7 @@
 package cn.vbyte.p2p;
 
+import android.net.Uri;
+
 import com.vbyte.p2p.IController;
 import com.vbyte.p2p.OnLoadedListener;
 
@@ -15,6 +17,11 @@ public abstract class BaseController implements IController {
     // load => unload => load 操作的异步转同步
     public static final int VIDEO_LIVE = 0;
     public static final int VIDEO_VOD = 1;
+    //同步总的次数是50次
+    public static final int syncRetryCount = 50;
+    //同步等待时间为100ms
+    public static final long syncWaitTime = 100;
+
     public static class LoadEvent {
         public int videoType;
         public String channel;
@@ -56,6 +63,63 @@ public abstract class BaseController implements IController {
     @Override
     public void load(String channel, String resolution, OnLoadedListener listener) throws Exception {
         load(channel, resolution, 0, listener);
+    }
+
+
+    protected volatile Uri mUri = null;
+    /**
+     * 同步获取uri接口
+     * @param channel 对直播是频道ID，对点播是资源链接
+     * @param resolution 统一为 "UHD"
+     * @return 取得的uri
+     * @throws Exception 当load/unload没有成对调用时，会抛出异常提示
+     */
+    @Override
+    public Uri loadAsync(String channel, String resolution) throws Exception {
+        OnLoadedListener listener = new OnLoadedListener() {
+            @Override
+            public void onLoaded(Uri uri) {
+                mUri = uri;
+            }
+        };
+        this.load(channel, resolution, listener);
+        int retryCount = syncRetryCount;
+
+        while (mUri == null && (retryCount--) > 0) {
+            Thread.sleep(syncWaitTime);
+        }
+        if (retryCount == 0) {
+            this.unload();
+        }
+        return mUri;
+    }
+
+    /**
+     * 同步获取uri接口
+     * @param channel 对直播是频道ID，对点播是资源链接
+     * @param resolution 统一为 "UHD"
+     * @param startTime 起始时间，直播是时移，点播即开始时间偏移
+     * @return 取得的uri
+     * @throws Exception 当load/unload没有成对调用时，会抛出异常提示
+     */
+    @Override
+    public Uri loadAsync(String channel, String resolution, double startTime) throws Exception {
+        OnLoadedListener listener = new OnLoadedListener() {
+            @Override
+            public void onLoaded(Uri uri) {
+                mUri = uri;
+            }
+        };
+        this.load(channel, resolution, startTime, listener);
+        int retryCount = syncRetryCount;
+
+        while (mUri == null && (retryCount--) > 0) {
+            Thread.sleep(syncWaitTime);
+        }
+        if (retryCount == 0) {
+            this.unload();
+        }
+        return mUri;
     }
 
     /**
